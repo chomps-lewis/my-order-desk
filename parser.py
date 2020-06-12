22517b606c6a1722b36235e3f9d4be4f27a91ec8
chomps-lewis <61520763+chomps-lewis@users.noreply.github.com>
2020-06-12 10:09:28 -0400

    Updated Parser

--- parser.py
+++ parser.py
@@ -6,9 +6,25 @@ counter = 0
 mytree = ET.parse(file_name)
 myroot = mytree.getroot()
 
+
+def get_qty(s):
+    # I had to put a try except because non-signage orders breaks everything
+    # TO DO either get better xml input (signage orders only) or make a rule
+    # Have script check the SKU, if not signage then pass
+
+    # This takes a string in this format 120429|4|0.0000|Y|0.0000|1|20|1|
+    # The second value after the | is the qty '4' here
+    try:
+        return s[int(s.index('|')+1)]
+    except:
+        print('Not a valid order')
+
 for child in myroot:
     try:
-        print(myroot[counter][5][-1][4][0].text)
+        print(myroot[counter][5][-1][4][0].text) # Grab SKU
+        print(myroot[counter][5][-1][4][5][6].text) # Grab Store #
+        print(get_qty(myroot[counter][5][-1][4][4][6].text))# Grab Qty
     except IndexError:
+        print('Something wrong happened')
         pass
     counter += 1
