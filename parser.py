881b0b52effe3140da966775fc0f5d4d121ea9d3
chomps-lewis <61520763+chomps-lewis@users.noreply.github.com>
2020-06-16 08:38:12 -0400

    Updated parser.py
    Still need to make sure it is always getting the right data, decide if the .xml will be cherry picked or if the script will handle irrelevant orders

--- parser.py
+++ parser.py
@@ -1,6 +1,6 @@
 from xml.etree import ElementTree as ET
 
-file_name = 'xml.xml'
+file_name = 'xml6624810.xml'
 counter = 0
 
 mytree = ET.parse(file_name)
@@ -17,13 +17,21 @@ def get_qty(s):
     try:
         return s[int(s.index('|')+1)]
     except:
-        print('Not a valid order')
+        print('There was an error parsing this string:\n' + str(s))
+
+
+def get_id(s):
+    try:
+        return s[0:5]
+    except:
+        print('There was an error parsing this string:\n' + str(s))
+
 
 for child in myroot:
     try:
-        print(myroot[counter][5][-1][4][0].text) # Grab SKU
-        print(myroot[counter][5][-1][4][5][6].text) # Grab Store #
-        print(get_qty(myroot[counter][5][-1][4][4][6].text))# Grab Qty
+        print((myroot[counter][5][-1][4][4][6].text))  # Grab SKU
+        print(myroot[counter][5][-1][4][5][6].text)  # Grab Store #
+        print((myroot[counter][5][-1][4][4][6].text))  # Grab Qty
     except IndexError:
         print('Something wrong happened')
         pass
