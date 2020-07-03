cfe778e37e690bb186b089a98b1ba75a3736f6e3
chomps-lewis <61520763+chomps-lewis@users.noreply.github.com>
2020-07-03 13:07:34 -0400

    Almost Done
    Last feature to implement is total sell amount for each store

--- playground.py
+++ playground.py
@@ -2,6 +2,8 @@ from table import prices, orders_by_store_dict
 from assign_order import assign_order
 
 orders = []
+invoice = []
+cost = 0
 
 with open('june.txt') as f:
     data = f.readlines()
@@ -31,17 +33,32 @@ for order in orders:
     # Format is: 4 Packs of 1up HG Sale @ $45/pack
     # We'll remember the line item as a string, s
     s = ''
-    s += order[1] + ' Packs of ' + (prices[(order[0])])[0] + ' @ $' + (prices[(order[0])])[1] + '/pack'
+
+    total_sell = 0 # Total amount the store bought in signage this month
+    total_q = 0 # Total amount of packs of signs bought by the store this month
+
+    q = order[1] # Number of packs ordered on one line item
+    form = (prices[(order[0])])[0]
+    sell = (prices[(order[0])])[1]
+
+    s += q + ' Packs of ' + form + ' @ $' + sell + '/pack'
+
+    # Need to record the cost of these packs as well
+    cost += int(q) * float((prices[(order[0])])[2])
+    # Need to record total # of packs and total sell for this store for the month
+    total_sell += int(q) * int(sell)
     # Now we need to assign this line item to the store that ordered it
     assign_order(order[2], s, orders_by_store_dict)
 
-# NEXT PROBLEM TO TACKLE
-# HOW TO TRANSFORM THE orders_by_store_dict into the format we need it in for the final invoice?
-
-invoice = []
-
 for store in orders_by_store_dict:
-    store_orders = []
-    store_orders.append(store)
-    store_orders.append(orders_by_store_dict[store])
+    if orders_by_store_dict[store]:
+        invoice.append(store + '\n')
+        for order in orders_by_store_dict[store]:
+            invoice.append(order + '\n')
+        invoice.append('\n')
+with open('invoice.txt', 'w') as f:
+    f.writelines(invoice)
+    f.write('OUR COST (Do not include on invoice): ' + str(cost))
+# OK so we got the invoice. Now we just need the COST DATA
+# ALSO should add the total sell amount for the
 
